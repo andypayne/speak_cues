776951ca42034241b87a8f8e322119e9beccb1ae
andypayne <apayne@gmail.com>
2020-04-05 15:43:48 -0700

    first

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Speak Cues
+
+Generate audio speech files for input text specified in a file. Each cue should
+be separated by a new line.
+
+## Usage
+
+```bash
+speak_cues.py [-h] [--in INFILE] [--out OUTDIR]
+
+optional arguments:
+  -h, --help    show this help message and exit
+  --in INFILE   The input file (default: ./cues.txt)
+  --out OUTDIR  The output directory (default: ./out/)
+```
+
---
+++ speak_cues.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""Generate audio speech files for input text."""
+
+import argparse
+import platform
+import re
+import os
+import subprocess
+
+
+SPEAK_CMDS = {
+    'Linux':  'espeak -v en-us -w "{}.wav" "{}"',
+    'Darwin': 'say -v Susan -o "{}.wav" --data-format=LEF32@22050 "{}"'
+}
+
+def cue_to_filename(cue):
+    """Given a cue as text, return a filename string. All characters other than
+    alphanumerics are replaced by underscores."""
+    return re.sub(r'[^A-Za-z0-9]', '_', cue)
+
+def gen_cues(infile, outdir, speak_cmd):
+    """Given an input file of text cues separated by newlines and an outdir,
+    generate sound files for each cue and store in outdir."""
+    os.makedirs(outdir, exist_ok=True)
+    with open(infile) as inf:
+        txt_cues = inf.read().split('\n')
+        for cue in txt_cues:
+            if len(cue) > 0:
+                cmd = speak_cmd.format(os.path.join(outdir,
+                                                    cue_to_filename(cue)), cue)
+                print('Running: ', cmd)
+                res = subprocess.check_output(cmd, shell=True)
+                print('Result: {}'.format(res))
+
+def main():
+    """Main function"""
+    arg_parser = argparse.ArgumentParser(description='Generate audio speech'
+                                         ' files for input text. Each cue should'
+                                         ' be separated by a new line.')
+    arg_parser.add_argument('--in', dest='infile', default='./cues.txt',
+                            help='The input file (default: ./cues.txt)')
+    arg_parser.add_argument('--out', dest='outdir', default='./out/',
+                            help='The output directory (default: ./out/)')
+    args = arg_parser.parse_args()
+    print(args)
+    print('infile = {}'.format(args.infile))
+    print('outdir = {}'.format(args.outdir))
+    print('OS     = {}'.format(platform.system()))
+    speak_cmd = SPEAK_CMDS[platform.system()] if platform.system() in SPEAK_CMDS else 'echo "No matching operating system"'
+    gen_cues(args.infile, args.outdir, speak_cmd)
+
+if __name__ == "__main__":
+    main()
